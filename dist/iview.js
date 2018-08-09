aa1a76fcd520d5c0cb8412e0df954c248b1b32c8
wangxin <wangxin.mt@bytedance.com>
2018-08-09 08:45:01 +0800

    add on-open-change for Select

--- dist/iview.js
+++ dist/iview.js
@@ -26266,7 +26266,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.0.2',
+    version: '3.0.3',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
